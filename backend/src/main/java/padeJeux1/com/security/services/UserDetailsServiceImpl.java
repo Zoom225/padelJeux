259d087e9bdf2d79a05b7e4b0623f798534fe8b3
Ruser <user@DESKTOP-NS5BTI0>
2026-04-09 18:04:42 +0200

    étape de la sécurité spring ok

---
+++ backend/src/main/java/padeJeux1/com/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package padeJeux1.com.security.services;
+
+import padeJeux1.com.model.User;
+import padeJeux1.com.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Service d'implémentation de UserDetailsService.
+ * Charge les détails de l'utilisateur à partir de la base de données.
+ */
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+}
